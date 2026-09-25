fbf44d2729c1f75f3faeb9973eafaa2738a9d8cf
Yana Sviridova <svzwyana075@mail.ru>
2026-09-25 12:50:43 +0200

    fix: build

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Suspense } from "react"
+
 import { UsersPage } from "@/modules/users"
 
 export default function Home() {
     return (
-        <>
+        <Suspense fallback={<p className="p-4">Loading users...</p>}>
             <UsersPage />
-        </>
+        </Suspense>
     )
 }
